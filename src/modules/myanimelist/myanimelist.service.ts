eedb8b23997a1bbe7e1bcf35b4bd052d4c9ec644
James Trotter <james@honestbank.com>
2023-08-13 12:47:33 -0400

    fix: passed data

--- src/modules/myanimelist/myanimelist.service.ts
+++ src/modules/myanimelist/myanimelist.service.ts
@@ -304,8 +304,22 @@ export class MyanimelistService {
       }
     })*/
     await page.setDefaultNavigationTimeout(60 * 2000)
-    await page.goto(`${url}/episodes`)
+    await page.goto(`${url}`)
     await this.handleCaptchas(page)
+
+    // get all links
+    const links: ElementHandle[] = await ClusterManager.findMany(page, 'a')
+
+    // select link with textContent 'Episodes'
+    const episodesLink = links.find((link: ElementHandle) => {
+      return page
+        .evaluate((el: any) => el.textContent, link)
+        .includes('Episodes')
+    })
+
+    // click episodes link
+    await episodesLink.click()
+    await page.waitForNavigation()
 
     const elements: ElementHandle[] = await ClusterManager.findMany(
       page,
